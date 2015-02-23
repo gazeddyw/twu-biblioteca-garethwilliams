a5e170b5e61e63ad5a217bde8a204eabdd42974c
Gareth Williams <gareth.williams987@gmail.com>
2015-02-23 17:16:45 +0000

    Add UserTest class to test User functionality.

---
+++ test/com/twu/biblioteca/UserTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+
+/**
+ * Created by Gareth Williams on 2/23/15.
+ */
+public class UserTest {
+
+    User myFirstUser;
+
+    @Before
+    public void setUp() throws Exception {
+        myFirstUser = new User();
+    }
+}
