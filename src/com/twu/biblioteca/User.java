1e86aedebd9dda36917b975adfc2c44dbcb173aa
Gareth Williams <gareth.williams987@gmail.com>
2015-02-23 17:31:09 +0000

    Add tests to UserTest for user creation and attribute checking.

---
+++ src/com/twu/biblioteca/User.java
@@ -0,0 +1,29 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by Gareth Williams on 2/23/15.
+ */
+public class User {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public User(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Gareth Williams on 2/23/15.
@@ -11,6 +14,21 @@ public class UserTest {
 
     @Before
     public void setUp() throws Exception {
-        myFirstUser = new User();
+        myFirstUser = new User("User One", "Email", "Phone");
+    }
+
+    @Test
+    public void testUserHasCorrectName() throws Exception {
+        assertEquals("User One", myFirstUser.getName());
+    }
+
+    @Test
+    public void testUserHasCorrectEmail() throws Exception {
+        myFirstUser.getEmail();
+    }
+
+    @Test
+    public void testUserHasCorrectPhoneNumber() throws Exception {
+        myFirstUser.getPhoneNumber();
     }
 }
