dedb26fec0d63c8eb4d7cfdf8474b9cffa0a7c6a
Gareth Williams <gareth.williams987@gmail.com>
2015-02-25 21:23:17 +0000

    Add test class for BibliotecaApp with skeleton test.

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+/**
+ * Created by Gareth Williams on 2/25/15.
+ */
+public class BibliotecaAppTest {
+
+    @Test
+    public void shouldCallInitMethod() throws Exception {
+    }
+}
